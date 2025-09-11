98e1ff8b7a86a0c47befe2ba6d531ac4144dcd73
jongbok654 <jongbok654@gmail.com>
2025-09-11 17:34:18 +0900

    2025-09-11 17:34 push

--- Spring09_JPA/src/main/java/com/example/spring09/controller/RestMemberController2.java
+++ Spring09_JPA/src/main/java/com/example/spring09/controller/RestMemberController2.java
@@ -1,7 +1,9 @@
 package com.example.spring09.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,16 @@ import lombok.RequiredArgsConstructor;
 public class RestMemberController2 {
 	
 	private final MemberService memberService;
+	
+	//회원 정보 전체 수정 요청 처리
+		@PutMapping("/members/{num}")
+		public MemberDto update(@PathVariable int num,@Valid  @RequestBody MemberDto dto) {//json 문자열이 전송되면 @RequestBody 어노테이션이 필요하다
+			//수정할 회원의 번호를 dto에 넣어준다
+			dto.setNum(num);
+			memberService.updateMember(dto);
+			return dto;
+		}
+	
 	
 	@PostMapping("/members")
 	public MemberDto create(@Valid @RequestBody MemberDto dto) {//@Valid 어노테이션을 추가해서 검증도 수행한다
